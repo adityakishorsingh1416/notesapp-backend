d985fe9d399b646ad762514543f6a32bd668f0bd
ADITYA <>
2025-12-10 16:51:55 +0530

    update backend config

--- server.js
+++ server.js
@@ -14,9 +14,13 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://notesappbyadiiii.netlify.app"
+  ],
   credentials: true
 }));
+
 
 app.use(session({
   secret: "yourSecretKey123",
@@ -39,4 +43,4 @@ mongoose.connect(process.env.MONGO_URL)
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/login`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
